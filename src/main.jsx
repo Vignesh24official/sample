935dd4a30acdfe4d95697b6d3d83094ba42346d3
Vignesh <vigneshvms24@gmail.com>
2026-03-04 18:08:26 +0530

    updtae

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,18 @@ import App from './App.jsx'
 // We don't need index.css if we are using App.css, 
 // but you can leave it if your vite setup created it.
 
+
+
+
+
+
+
+
+
+
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
